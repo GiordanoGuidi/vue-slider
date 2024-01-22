e132216547c0a43617692e48630fa48cbb8be323
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-01-22 18:00:27 +0100

    added setinterval

--- carousel_base/js/script.js
+++ carousel_base/js/script.js
@@ -4,23 +4,35 @@ const app = createApp({
        return{
         destinations,
         currentIndex : 0,
+        interval:null,
        }
     },
     computed:{
 
     },
     methods:{
+        //FUNZIONE PER AUMENTARE L'INDICE
         goNextIndex(){
             if(this.currentIndex === this.destinations.length -1){
                this.currentIndex = 0;
             }else this.currentIndex ++;
         },
 
+        //FUNZIONE PER DIMINUIRE L'INDICE
         goPrevIndex(){
             if(this.currentIndex === 0){
                 this.currentIndex = this.destinations.length -1
             } else this.currentIndex --;
+        },
+
+        stopAutoPlay(){
+            clearInterval(this.interval)
         }
+    },
+    mounted(){
+        this.interval = setInterval(()=>{
+            this.goNextIndex()
+        },1000)
     }
     
 })
