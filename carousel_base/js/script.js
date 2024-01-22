7532394031c1fbcb373a7e67db65f82f7b6c278d
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-01-22 17:09:57 +0100

    added methods on buttons

--- carousel_base/js/script.js
+++ carousel_base/js/script.js
@@ -5,7 +5,24 @@ const app = createApp({
         destinations,
         currentIndex : 0,
        }
+    },
+    computed:{
+
+    },
+    methods:{
+        goNextIndex(){
+            if(this.currentIndex === this.destinations.length -1){
+               this.currentIndex = 0;
+            }else this.currentIndex ++;
+        },
+
+        goPrevIndex(){
+            if(this.currentIndex === 0){
+                this.currentIndex = this.destinations.length -1
+            } else this.currentIndex --;
+        }
     }
+    
 })
 app.mount('#root')
 
